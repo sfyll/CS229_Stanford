335cc649f6af6a9159514baa499230dda1d2e7d4
SFYLL <39958632+SFYLL@users.noreply.github.com>
2020-04-08 17:08:39 +0200

    Class Finalized

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -51,10 +51,14 @@ error_val   = zeros(m, 1);
 %       end
 %
 
+
 % ---------------------- Sample Solution ----------------------
 
-
-
+for i=1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end    
 
 
 
--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -38,10 +38,16 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
+for i = 1:length(lambda_vec)
+    theta = trainLinearReg(X, y, lambda_vec(i));
+    error_train(i) = linearRegCostFunction(X, y, theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
-
-
+disp(X)
+disp(size(X))
+disp(size(Xval))
 
 
 
